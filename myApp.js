7306c9d3869e3e298b3e725be36cbc3f3eb9a61b
bhu <harold9188@gmail.com>
2021-09-25 12:15:40 -0700

    get / path

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
 var express = require('express');
 var app = express();
 
+console.log('App is starting'); 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
