197c450b0c612e8a6abb96d8af43858c44ac2b2c
bhu <harold9188@gmail.com>
2021-09-25 14:27:08 -0700

    req method, path, ip

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ if (!process.env.DISABLE_XORIGIN) {
 
 app.use('/public', express.static(__dirname + '/public')); 
 
+app.use( (req, res, next) => {
+  var userInfo = req.method + " " + req.path + " - " + req.ip;
+  console.log(userInfo);
+  next();
+}); 
+
 app.get('/', (req, res) => {
   let absolutePath = __dirname + '/views/index.html'
   res.sendFile(absolutePath); 
